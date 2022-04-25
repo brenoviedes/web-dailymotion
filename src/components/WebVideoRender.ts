8999a2c6707ba2343baa46c9675377c0c51d88d2
Breno Viedes <breno_viedes@hotmail.com>
2022-04-25 18:28:11 -0400

    ajustando css

--- src/components/WebVideoRender.ts
+++ src/components/WebVideoRender.ts
@@ -6,13 +6,13 @@ export const renderSelectedVideo = (video: webVideo, container: HTMLElement) => 
 
     <div id="videoRendered"> 
 
-        <h2>${video.title}</h2>
+        <iframe style="width:100%; height:100%; position:absolute;left:0px;top:0px; overflow:hidden" frameborder="0" type="text/html" src="https://www.dailymotion.com/embed/video/${video.id}?autoplay=1" width="100%" height="100%" allowfullscreen allow="autoplay">
+        
+        </iframe>
 
         <br><br>
 
-        <iframe style="width:100%; height:auto; position:absolute; left:0px; top: 50px;overflow:hidden" frameborder="0" type="text/html" src="https://www.dailymotion.com/embed/video/${video.id}?autoplay=1" width="100%" height="100%" allowfullscreen allow="autoplay">
-        
-        </iframe>
+        <h2>${video.title}</h2>
     
     </div>
     `
