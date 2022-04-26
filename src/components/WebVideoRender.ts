08b27a9776140374a1f4548d5e66d7c09ea04f9d
Breno Viedes <breno_viedes@hotmail.com>
2022-04-25 23:01:43 -0400

    ajuste css

--- src/components/WebVideoRender.ts
+++ src/components/WebVideoRender.ts
@@ -5,16 +5,13 @@ export const renderSelectedVideo = (video: webVideo, container: HTMLElement) => 
     const htmlContent = `
 
     <div id="videoRendered"> 
-
-        <iframe style="width:100%; height:100%; position:absolute;left:0px;top:0px; overflow:hidden" frameborder="0" type="text/html" src="https://www.dailymotion.com/embed/video/${video.id}?autoplay=1" width="100%" height="100%" allowfullscreen allow="autoplay">
-        
-        </iframe>
-
-        <br><br>
-
-        <h2>${video.title}</h2>
-    
+            <iframe style="width:100%; height:100%; position:absolute;top:50px; overflow:hidden" frameborder="0" type="text/html" src="https://www.dailymotion.com/embed/video/${video.id}?autoplay=1" margin="0 auto" width="100%" height="100%" allowfullscreen allow="autoplay">
+            </iframe>
     </div>
+
+    <br>
+
+    <h2>${video.title}</h2>
     `
 
     container.innerHTML = htmlContent
@@ -32,3 +29,5 @@ export const renderSelectedVideo = (video: webVideo, container: HTMLElement) => 
     
 //     </div>
 
+// <iframe style="width:80%; height:100%; position:absolute;left:0px;top:0px; overflow:hidden" frameborder="0" type="text/html" src="https://www.dailymotion.com/embed/video/${video.id}?autoplay=1" width="100%" height="100%" allowfullscreen allow="autoplay">
+// </iframe>
